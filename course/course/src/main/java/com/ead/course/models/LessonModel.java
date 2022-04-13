8c1ae6d6eec26710b2df57e3273856cc5a003af9
iaiá <iaiaselva.is@gmail.com>
2022-04-13 10:18:39 -0300

    crud dos courses e modules

--- course/course/src/main/java/com/ead/course/models/LessonModel.java
+++ course/course/src/main/java/com/ead/course/models/LessonModel.java
@@ -32,4 +32,7 @@ public class LessonModel implements Serializable {
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private ModuleModel module;
 }
--- course/course/src/main/java/com/ead/course/models/ModuleModel.java
+++ course/course/src/main/java/com/ead/course/models/ModuleModel.java
@@ -1,12 +1,16 @@
 package com.ead.course.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Data
@@ -29,5 +33,12 @@ public class ModuleModel implements Serializable {
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private CourseModel course;
+
+    @OneToMany(mappedBy = "module", fetch = FetchType.LAZY)
+    @Fetch(FetchMode.SUBSELECT)
+    private Set<LessonModel> lessons;
 
 }
