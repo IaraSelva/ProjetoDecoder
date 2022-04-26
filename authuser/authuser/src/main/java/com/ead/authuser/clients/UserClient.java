ff31e8c5c45ba11991ddae0040f2cd75fe571391
iaiá <iaiaselva.is@gmail.com>
2022-04-26 19:38:31 -0300

    implementando comunicação com o microservice de cursos configurando pageable

---
+++ authuser/authuser/src/main/java/com/ead/authuser/clients/UserClient.java
@@ -0,0 +1,52 @@
+package com.ead.authuser.clients;
+
+import com.ead.authuser.dto.CourseDto;
+import com.ead.authuser.dto.ResponsePageDto;
+import com.ead.authuser.services.UtilsService;
+import com.ead.authuser.services.impl.UtilsServiceImpl;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@Log4j2
+public class UserClient {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    UtilsServiceImpl utilsService;
+
+    String REQUEST_URL_COURSE = "http://localhost:8082";
+
+    public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
+        List<CourseDto> searchResult = null;
+        ResponseEntity<ResponsePageDto<CourseDto>> result = null;
+        String url = REQUEST_URL_COURSE + utilsService.createUrlGetAllCoursesByUser(userId, pageable);
+        log.debug("Request URL: {} ", url);
+        log.info("Request URL: {} ", url);
+        try{
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType =
+                    new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+            result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            searchResult = result.getBody().getContent();
+            log.debug("Response Number of Elements: {} ", searchResult.size());
+        } catch (HttpStatusCodeException e){
+            log.error("Error request /courses {} ", e);
+        }
+        log.info("Ending request /courses userId {} ", userId);
+        return new PageImpl<>(searchResult);
+    }
+}
---
+++ authuser/authuser/src/main/java/com/ead/authuser/enums/CourseLevel.java
@@ -0,0 +1,5 @@
+package com.ead.authuser.enums;
+
+public enum CourseLevel {
+    ADMIN, STUDENT, INSTRUCTOR
+}
